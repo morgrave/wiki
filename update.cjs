03e1a7ce5179b6ed449312f3c952c485eff5b0fc
dwaldo <dwaldo@naver.com>
2025-12-18 07:56:08 +0900

    deploy

--- update.cjs
+++ update.cjs
@@ -192,7 +192,7 @@ async function main() {
   console.log(`🏷️ 라벨: ${label}`);
 
   // 구글 AI 스튜디오 접속
-  const browser = await chromium.launchPersistentContext("./user_data/7", {
+  const browser = await chromium.launchPersistentContext("./user_data/8", {
     headless: false,
     args: ["--disable-blink-features=AutomationControlled"],
     viewport: { width: 2560, height: 1080 },
@@ -237,13 +237,13 @@ async function main() {
   //   "https://aistudio.google.com/u/1/prompts/18cXzpP80m6cwSq-GcsnfK0zyApbRXRcM"
   // );
   // 듀얼단 7 (korea)
-  await page.goto(
-    "https://aistudio.google.com/u/1/prompts/1vkHW882mtbEfjIdpK42Jg_LOUiZVvTOR"
-  );
-  // 듀얼단 8 (arm6)
   // await page.goto(
-  //   "https://aistudio.google.com/u/1/prompts/1fbB38xzGoF8ylmS2o_M23S5cxUld-gQY"
+  //   "https://aistudio.google.com/u/1/prompts/1vkHW882mtbEfjIdpK42Jg_LOUiZVvTOR"
   // );
+  // 듀얼단 8 (arm6)
+  await page.goto(
+    "https://aistudio.google.com/u/1/prompts/1fbB38xzGoF8ylmS2o_M23S5cxUld-gQY"
+  );
   // 듀얼단 9 (arm7)
   // await page.goto(
   //   "https://aistudio.google.com/u/1/prompts/1O8zK5oNh79tEh9wE3ILt2yWrGM33gbcn"

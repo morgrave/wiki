be4e8d84930436d596608b3ac31b81ec82094ef2
dwaldo <dwaldo@naver.com>
2026-01-10 16:05:51 +0900

    deploy

--- update.cjs
+++ update.cjs
@@ -192,7 +192,7 @@ async function main() {
   console.log(`🏷️ 라벨: ${label}`);
 
   // 구글 AI 스튜디오 접속
-  const browser = await chromium.launchPersistentContext("./user_data/2", {
+  const browser = await chromium.launchPersistentContext("./user_data/1", {
     headless: false,
     args: ["--disable-blink-features=AutomationControlled"],
     viewport: { width: 2560, height: 1080 },
@@ -201,9 +201,9 @@ async function main() {
   const pages = browser.pages();
   const page = pages.length > 0 ? pages[0] : await browser.newPage();
   // 패스파인더
-  //  await page.goto(
-  //    "https://aistudio.google.com/u/1/prompts/1XvpEt1Ygr9EKB8SA9aNuQfRH7VuGJgO-",
-  //  );
+   await page.goto(
+     "https://aistudio.google.com/u/1/prompts/1XvpEt1Ygr9EKB8SA9aNuQfRH7VuGJgO-",
+   );
   // 시트론 1
   // await page.goto(
   //   "https://aistudio.google.com/u/1/prompts/17T-ly8tPyFqyKY9ZASsvQ6FL3sOUfLsa",
@@ -217,9 +217,9 @@ async function main() {
   //   "https://aistudio.google.com/u/1/prompts/1A8cN9pED4TdlWozYfjfQFBi4_YMwRCwb"
   // );
   // 듀얼단 2
-  await page.goto(
-    "https://aistudio.google.com/u/1/prompts/1-T1Rv1SMl0TjQBG7t7L_3aWEnb1RS170"
-  );
+  // await page.goto(
+  //   "https://aistudio.google.com/u/1/prompts/1-T1Rv1SMl0TjQBG7t7L_3aWEnb1RS170"
+  // );
   // 듀얼단 3 (arm1)
   // await page.goto(
   //   "https://aistudio.google.com/u/1/prompts/1Bq-4hbsYWimlOrCbfqZ1lGJ-oDEBr9cs"
@@ -255,13 +255,13 @@ async function main() {
   }
 
   // 이전 대화 내역 전부 삭제
-  // await eraseChatLog(page);
+  await eraseChatLog(page);
 
   // KB.txt 내용 입력
-  // await writeKBFile(page, folderPath);
+  await writeKBFile(page, folderPath);
 
   // 사용자가 선택한 로그 내용 입력
-  // await writeLogFile(page, label, logContent);
+  await writeLogFile(page, label, logContent);
 
   // 업데이트된 지식 베이스 파일 목록 추출 및 반영
   await processMDList(page, folderPath, label);

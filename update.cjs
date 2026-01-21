426dd9ba97a6bec2427722ebbee0df117c9cbe3a
dwaldo <dwaldo@naver.com>
2026-01-21 21:20:55 +0900

    deploy

--- update.cjs
+++ update.cjs
@@ -209,7 +209,7 @@ async function main() {
   console.log(`🏷️ 라벨: ${label}`);
 
   // 구글 AI 스튜디오 접속
-  const browser = await chromium.launchPersistentContext("./user_data/9", {
+  const browser = await chromium.launchPersistentContext("./user_data/1", {
     headless: false,
     args: ["--disable-blink-features=AutomationControlled"],
     viewport: { width: 2560, height: 1080 },
@@ -230,9 +230,9 @@ async function main() {
   //   "https://aistudio.google.com/u/1/prompts/1JAU4fxM4WeuR3-6YNx6dYquzAYnajQze",
   // );
   // 페리아
-  // await page.goto(
-  //   "https://aistudio.google.com/u/1/prompts/1A8cN9pED4TdlWozYfjfQFBi4_YMwRCwb"
-  // );
+  await page.goto(
+    "https://aistudio.google.com/u/1/prompts/1A8cN9pED4TdlWozYfjfQFBi4_YMwRCwb"
+  );
   // 페리아 2
   // await page.goto(
   //   "https://aistudio.google.com/u/1/prompts/1-T1Rv1SMl0TjQBG7t7L_3aWEnb1RS170"
@@ -262,9 +262,9 @@ async function main() {
   //   "https://aistudio.google.com/u/1/prompts/1fbB38xzGoF8ylmS2o_M23S5cxUld-gQY"
   // );
   // 페리아 9 (arm7)
-  await page.goto(
-    "https://aistudio.google.com/u/1/prompts/1O8zK5oNh79tEh9wE3ILt2yWrGM33gbcn"
-  );
+  // await page.goto(
+  //   "https://aistudio.google.com/u/1/prompts/1O8zK5oNh79tEh9wE3ILt2yWrGM33gbcn"
+  // );
   await page.waitForSelector("textarea", { timeout: 60000 });
 
   if (label === "") {
